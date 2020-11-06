0e122ca13c420041491f469b4b3b956f5e887f65
Moisés Mariano <moisesduartem@gmail.com>
2020-11-06 19:57:10 -0300

    commands father with two attr examples

---
+++ raj-cli/Command.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Raj;
+
+class Command
+{
+    public array $arguments = ['command', 'cmd'];
+    public array $descriptions = [
+        'x' => 'what command/cmd x do',
+        'y' => 'what command/cmd y do'
+    ];
+}
