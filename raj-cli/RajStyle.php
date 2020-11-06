da5bae3a78d4ef9bbc006c9c46a2e7fe572d85bc
Moisés Mariano <moisesduartem@gmail.com>
2020-11-06 19:57:56 -0300

    configure stylization details and --help dynamics

---
+++ raj-cli/RajStyle.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace Raj;
+
+abstract class RajStyle
+{
+    protected function commandSummary() : void
+    {
+        $this->climate->green()->text("Raj, the Lazlo's Command Line Interface\n");
+        $this->renderSummary();
+        echo "\n";
+    }
+
+    protected function renderSummary() : void
+    {
+        foreach ($this->commands as $command) {
+            $methods = get_class_methods($command);
+            $this->climate->yellow()->columns([implode(', ', $command->arguments)]);
+            foreach ($methods as $method) {
+                $this->climate->cyan()->columns([[$method, $command->descriptions[$method]]]);
+            }
+        }
+    }
+}
